df736ac829f2795b481702b504e80551362dab16
zhang-cb <1037475388@qq.com>
2022-03-26 16:53:17 +0800

    IOstudy1

---
+++ day09/src/com/atguigu/java/TestIO.java
@@ -0,0 +1,21 @@
+package com.atguigu.java;
+
+import java.io.File;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author zhangcb
+ * @Date 2021-3-2 17:43
+ * @Version 1.0
+ **/
+public class TestIO {
+    public static void main(String[] args) {
+        File file = new File("hello.txt");//相较于当前工程
+        System.out.println(file.getAbsolutePath());
+
+        File file1 = new File("day09\\hello.txt");
+        System.out.println(file1.getAbsolutePath());
+
+    }
+}
